1ff2ae54fb9382a101ec122e0783d79f7f27453c
David Moran <dave.moran118@gmail.com>
2016-11-01 10:10:22 -0400

    Initial Commit

---
+++ rbm-staff.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ Plugin Name: RBM Staff
+ Plugin URI: http://reachbeyondmarketing.com
+ Description: A plugin to display the Reach Beyond Marketing staff in an easier manner
+ Version: 0.1
+ Author: Dave M
+ Author URI: https://dmoran.co
+ License: GPL2
+ */
