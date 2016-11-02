6ae3a7acc8138da3d4646ccdc240457b0cfe9d27
Dave Moran <dave.moran118@gmail.com>
2016-11-02 10:24:45 -0400

    Format page to begin adding skills

--- settings.php
+++ settings.php
@@ -14,10 +14,57 @@ function rbm_staff_menu() {
 }
 
 function rbm_staff_options() {
-  if (!current_user_can('manage_options') ) {
-    wp_die( __( 'You do not have sufficient permissions to access this page.') );
+  //must check that the user has the required capability
+  if (!current_user_can('manage_options')) {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
   }
-  echo '<div class="wrap">';
-  echo '<p>Here is where the form would go if I actually had options.</p>';
-  echo '</div>';
-}
+
+  // variables for the field and option names
+  $skills = (object) [];
+  $hidden_field_name = 'mt_submit_hidden';
+  $data_field_name = 'mt_favorite_color';
+
+    // Read in existing option value from database
+    $opt_val = get_option( $skills );
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+    // Read their posted value
+        $opt_val = $_POST[ $data_field_name ];
+
+        // Save the posted value in the database
+        update_option( $opt_name, $opt_val );
+
+        // Put a "settings saved" message on the screen
+
+?>
+    <div class="updated"><p><strong><?php _e('settings saved.', 'menu-test' ); ?></strong></p></div>
+    <?php }
+    echo '<div class="wrap">';
+    // header
+    echo "<h2>" . __( 'Menu Test Plugin Settings', 'menu-test' ) . "</h2>";
+    ?>
+
+    <form name="form1" method="post" action="">
+      <input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+      <table class="form-table">
+        <tbody>
+          <tr>
+            <th scope="row">
+              <label>Available Skills</label>
+            </th>
+            <td>
+              <a href="#" class="button">Add New Skill</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+
+      <p class="submit">
+        <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+      </p>
+    </form>
+  </div>
+<?php }
